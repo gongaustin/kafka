49e149927aaa44651e7cfe2cb8dba04e6848757a
GongJun <28402674@qq.com>
2019-01-23 15:53:20 +0800

    Kafka示例代码

--- src/main/java/com/gongjun/kafka/KafkaApplication.java
+++ src/main/java/com/gongjun/kafka/KafkaApplication.java
@@ -1,9 +1,13 @@
 package com.gongjun.kafka;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "com.gongjun.kafka")
 public class KafkaApplication {
 
     public static void main(String[] args) {
